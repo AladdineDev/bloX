e217fc56ce6cd94b4a722e0ce145c7c795d55fe9
AladdineDev <aaldn.dev@gmail.com>
2024-07-08 22:55:54 +0200

    feat: create tweet list screen

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:blox/features/tweet/views/tweet_list_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "BloX",
+      home: TweetListScreen(),
+    );
+  }
+}
---
+++ lib/features/tweet/views/tweet_list_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TweetListScreen extends StatelessWidget {
+  const TweetListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
