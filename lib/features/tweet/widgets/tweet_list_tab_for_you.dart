b406dc08ce145dd24da5fbec34b7d61cd406279d
AladdineDev <aaldn.dev@gmail.com>
2024-07-12 19:28:41 +0200

    feat: create tweet list tabs

---
+++ lib/features/tweet/widgets/tweet_list_tab_for_you.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TweetListTabForYou extends StatelessWidget {
+  const TweetListTabForYou({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("TweetListTabForYou"),
+    );
+  }
+}
