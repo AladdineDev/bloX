dd5da8551c1a574328c7fa28bf5ce79f235aaaf7
AladdineDev <aaldn.dev@gmail.com>
2024-07-13 18:06:24 +0200

    feat: create tweet creation screen

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,4 +1,5 @@
 import 'package:blox/features/auth/views/signup_screen.dart';
+import 'package:blox/features/tweet/views/tweet_creation_screen.dart';
 import 'package:blox/features/tweet/views/tweet_list_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -35,5 +36,29 @@ class SignupScreenRoute extends GoRouteData {
   @override
   Widget build(BuildContext context, GoRouterState state) {
     return const SignupScreen();
+  }
+}
+
+@TypedGoRoute<TweetCreationScreenRoute>(path: '/new-tweet')
+@immutable
+class TweetCreationScreenRoute extends GoRouteData {
+  @override
+  CustomTransitionPage buildPage(BuildContext context, GoRouterState state) {
+    return CustomTransitionPage(
+      key: state.pageKey,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(0.0, 1.0);
+        const end = Offset.zero;
+        var curve = Curves.easeOut;
+        var curveTween = CurveTween(curve: curve);
+        final tween = Tween(begin: begin, end: end).chain(curveTween);
+        final offsetAnimation = animation.drive(tween);
+        return SlideTransition(
+          position: offsetAnimation,
+          child: child,
+        );
+      },
+      child: const TweetCreationScreen(),
+    );
   }
 }
--- lib/features/tweet/views/tweet_list_screen.dart
+++ lib/features/tweet/views/tweet_list_screen.dart
@@ -1,5 +1,6 @@
 import 'package:blox/core/common/widgets/my_sliver_app_bar.dart';
 import 'package:blox/core/common/widgets/my_drawer.dart';
+import 'package:blox/core/router/router.dart';
 import 'package:blox/features/tweet/widgets/tweet.dart';
 import 'package:blox/features/tweet/widgets/tweet_list_tab_bar.dart';
 import 'package:blox/features/tweet/widgets/tweet_list_tabs.dart';
@@ -69,12 +70,16 @@ class TweetListScreen extends StatelessWidget {
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () => _onFloationgActionButtonPressed(context),
           child: const Icon(
             Icons.edit_outlined,
           ),
         ),
       ),
     );
+  }
+
+  void _onFloationgActionButtonPressed(BuildContext context) {
+    TweetCreationScreenRoute().push(context);
   }
 }
