a326ff3ddfb8513eca2ec6e1368596297b4300f3
AladdineDev <aaldn.dev@gmail.com>
2024-07-15 01:17:46 +0200

    fix: theme

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -133,4 +133,15 @@ final darkTheme = ThemeData.dark().copyWith(
       ),
     ),
   ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: FilledButton.styleFrom(
+      backgroundColor: Colors.white,
+      foregroundColor: Colors.black,
+      textStyle: const TextStyle(
+        fontSize: 18,
+        color: Colors.black,
+        fontWeight: FontWeight.w700,
+      ),
+    ),
+  ),
 );
