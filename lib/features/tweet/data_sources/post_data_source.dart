52532fef75b40f219265672985a41726a019edd9
AladdineDev <aaldn.dev@gmail.com>
2024-07-17 20:59:04 +0200

    feat: create data sources

---
+++ lib/features/tweet/data_sources/post_data_source.dart
@@ -0,0 +1,9 @@
+import 'package:blox/features/tweet/models/post.dart';
+
+abstract class PostDataSource {
+  Future<void> createPost({required Post post});
+  Stream<List<Post>> getPosts({required int limit});
+  Stream<Post?> getPost({required PostId postId});
+  Future<void> updatePost({required Post post});
+  Future<void> deletePost({required PostId postId});
+}
---
+++ lib/features/tweet/data_sources/remote_post_data_source.dart
@@ -0,0 +1,54 @@
+import 'package:blox/core/extensions/firebase_firestore_extension.dart';
+import 'package:blox/features/tweet/data_sources/post_data_source.dart';
+import 'package:blox/features/tweet/models/post.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class RemotePostDataSource extends PostDataSource {
+  RemotePostDataSource();
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  static const postsCollectionPath = 'posts';
+  static String postPath({required PostId postId}) {
+    return '$postsCollectionPath/$postId';
+  }
+
+  @override
+  Future<void> createPost({required Post post}) async {
+    final postsCollection = _firestore.postsCollection(this);
+    await postsCollection.add(post);
+  }
+
+  @override
+  Stream<List<Post>> getPosts({required int limit}) {
+    final postsCollection = _firestore.postsCollection(this);
+    final postsQuery = postsCollection.limit(limit);
+    return postsQuery.snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) => doc.data()).toList();
+    });
+  }
+
+  @override
+  Stream<Post?> getPost({required PostId postId}) {
+    final postDoc = _firestore.postDocument(
+      documentPath: postPath(postId: postId),
+    );
+    return postDoc.snapshots().map((snapshot) => snapshot.data());
+  }
+
+  @override
+  Future<void> updatePost({required Post post}) async {
+    final postDoc = _firestore.postDocument(
+      documentPath: postPath(postId: post.id!),
+    );
+    await postDoc.update(post.toJson());
+  }
+
+  @override
+  Future<void> deletePost({required PostId postId}) async {
+    final postDoc = _firestore.postDocument(
+      documentPath: postPath(postId: postId),
+    );
+    await postDoc.delete();
+  }
+}
