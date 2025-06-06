89d66b531d58c535a2830e9fb567592308ad4ae4
AladdineDev <aaldn.dev@gmail.com>
2025-06-06 21:13:04 +0200

    fix: firebase ios setup

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -63,7 +63,8 @@ class DefaultFirebaseOptions {
     messagingSenderId: '284801421562',
     projectId: 'esgi-tweet-4339f',
     storageBucket: 'esgi-tweet-4339f.appspot.com',
-    iosClientId: '284801421562-e85gp91bf08i2dfchu8tmnlcmf3364vf.apps.googleusercontent.com',
+    iosClientId:
+        '284801421562-e85gp91bf08i2dfchu8tmnlcmf3364vf.apps.googleusercontent.com',
     iosBundleId: 'com.example.blox',
   );
 }
